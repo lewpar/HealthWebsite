f7b211a9f1550fef539795bce39a97128afe422f
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-16 17:57:10 +1000

    Added script to show daily tip for the day.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
-function setDailyTip()
+async function setDailyTip()
 {
-    console.log("Hello World!");
+    try {
+        let days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+        const d = new Date();
+        let day = d.getDay();
+        let file = `/dailytips/${days[day - 1]}.html`
+
+        let result = await fetch(file);
+        if(result.ok)
+        {
+            let page = document.getElementsByClassName("page")[0];
+            let tipHTML = await result.text();
+    
+            page.innerHTML = tipHTML;
+        }
+    }
+    catch(exception) {
+        console.log(exception);
+    }
 }
