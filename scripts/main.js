b218737ce49abc4fb91f6235a55122d9ed9b00f0
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-16 20:38:13 +1000

    Fixed pathing issue.

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ async function setDailyTip()
         let days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
         const now = new Date();
         let day = now.getDay();
-        let filePath = `/dailytips/${days[day - 1]}.html`
+        let filePath = `./dailytips/${days[day - 1]}.html`
 
         let result = await fetch(filePath);
         if(result.ok)
