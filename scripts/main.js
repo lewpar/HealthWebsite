9b1ee7bcb5a42196413e9f1a4f04b8872bf41fe3
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-19 09:16:18 +1000

    Added next-prev buttons for tips page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
+let tipIndex = 0;
 
+function prevTip()
+{
+    tipIndex--;
+    setDailyTip();
+}
+
+function nextTip()
+{
+    tipIndex++;
+    setDailyTip();
+}
 /*
     Gets the day of the week page and injects it into the page element.
 */
@@ -6,12 +18,34 @@ async function setDailyTip()
 {
     try 
     {
+        let prev = document.getElementById("tip-prev");
+        let next = document.getElementById("tip-next");
+
         // The days of the week collection.
         let days = [ "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
         const now = new Date();
 
         // Get the number day of the week, sunday:0, saturday:6
-        let day = now.getDay();
+        let day = now.getDay() + tipIndex;
+        console.log(day);
+
+        if(day == days.length - 1)
+        {
+            next.setAttribute("disabled", "");
+        }
+        else 
+        {
+            next.removeAttribute("disabled");
+        }
+
+        if(day == 0)
+        {
+            prev.setAttribute("disabled", "");
+        }
+        else
+        {
+            prev.removeAttribute("disabled");
+        }
 
         // Get the name of the day using the index location.
         let filePath = `./dailytips/${days[day]}.html`
