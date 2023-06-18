97283f14e374f55236db56f7071cec1a7e892dd8
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-19 08:51:04 +1000

    Added comments as per requirements.

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,40 @@
+
+/*
+    Gets the day of the week page and injects it into the page element.
+*/
 async function setDailyTip()
 {
     try 
     {
+        // The days of the week collection.
         let days = [ "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
         const now = new Date();
+
+        // Get the number day of the week, sunday:0, saturday:6
         let day = now.getDay();
 
+        // Get the name of the day using the index location.
         let filePath = `./dailytips/${days[day]}.html`
 
+        // Fetch the daily tip page content.
         let result = await fetch(filePath);
+
+        // Check the HTTP response status is OK(200).
         if(result.ok)
         {
+            // Get the page element.
             let page = document.getElementsByClassName("page")[0];
+
+            // Get the HTML content from the daily tip page.
             let tipHTML = await result.text();
-    
+            
+            // Inject the daily tip HTML into the current page element.
             page.innerHTML = tipHTML;
         }
     }
     catch(exception) 
     {
+        // Log any exceptions caught into the browsers console.
         console.log(exception);
     }
 }
